c1f762d1785829d6974d2bc9d3f4de15ebb400c9
minmul <sksmstmdrud@gmail.com>
2026-06-28 23:08:34 +0900

    feat(design): 앱 색상 테마 적용

--- app/src/main/java/com/chalkak/recap/core/design/theme/Color.kt
+++ app/src/main/java/com/chalkak/recap/core/design/theme/Color.kt
@@ -2,10 +2,14 @@ package com.chalkak.recap.core.design.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val RecapBlue900 = Color(0xFF1F2937)
+val RecapBlue500 = Color(0xFF3845C7)
+val RecapBlue300 = Color(0xFF5C6DFF)
+val RecapBlue50 = Color(0xFFEFF0FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val RecapGray900 = Color(0xFF0B111D)
+val RecapGray700 = Color(0xFF222B3C)
+val RecapGray500 = Color(0xFF4D586C)
+val RecapGray300 = Color(0xFF99A0B0)
+val RecapGray200 = Color(0xFFCED2DE)
+val RecapGray100 = Color(0xFFE2E6ED)
