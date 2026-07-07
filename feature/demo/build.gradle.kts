2234fe2d005311184e0e8fd6fd32a98ad3b75628
minmul <sksmstmdrud@gmail.com>
2026-07-07 16:44:22 +0900

    fix(dependencies): 누락 의존성 추가

--- feature/demo/build.gradle.kts
+++ feature/demo/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
